62c549c9fd252b829fc986407a38976e14bb38da
gusleaooliveira <gus.leaooliveira@gmail.com>
2020-04-09 10:06:27 -0300

    Hello World! Criado em node

--- aula05/server.js
+++ aula05/server.js
@@ -5,5 +5,10 @@ const http = require('http');
 let app = express();
 
 app.get('/', (req, res) => {
-  return {'mgs': 'Hello, Cruel world!'}
+  res.status(200).send('Hello, Cruel World!');
+});
+
+
+http.createServer(app).listen(8001, () => {
+  console.log('Acesse: http://127.0.0.1:8001/');
 });
