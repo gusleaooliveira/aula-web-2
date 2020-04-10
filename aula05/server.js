97571bbd83fbdd02fdf57d1b02e93c387fdb850c
gusleaooliveira <gus.leaooliveira@gmail.com>
2020-04-09 21:22:43 -0300

    servidor da api criado

--- aula05/server.js
+++ aula05/server.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const http = require('http');
-
 
 let app = express();
 
+let tarefas = [
+  { 'tarefa': "Hello, Cruel World!" },
+  { 'tarefa': "Acrescentar um Banco de dados" },
+  { 'tarefa': "Tomar café" }
+];
+
+app.use(bodyParser.json());
+
+app.get('/tarefas', (req, res) => {
+  res.status(200).send(tarefas);
+});
+
+app.get('/tarefas/:id', (req, res) => {
+  res.status(200).send(tarefas[req.params.id]);
+});
+
+app.post('/tarefas', (req, res) => {
+  tarefas.push(req.body);
+  res.status(200).send(tarefas);
+});
+
+app.put('/tarefas/:id', (req, res) => {
+  tarefas[req.params.id] = req.body;
+  res.status(200).send(tarefas);
+});
+
+app.delete('/tarefas/:id', (req, res) => {
+  delete tarefas[req.params.id];
+  res.status(200).send(tarefas);
+});
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, Cruel World!');
+  res.status(404).send('Você está na base!');
 });
 
 
